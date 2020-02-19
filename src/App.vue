e9b36a6aa3b9112225ee4b8dda80841ca106d6be
Lucas Gomes <lucasgomesp22@gmail.com>
2020-02-19 18:36:45 -0300

    adding appointment list component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,33 @@
 <template>
   <div id="main-app" class="container">
     <h4>{{ title }}</h4>
-    <font-awesome-icon icon="plus" class="mr-2"/>Add Appointment
+    <font-awesome-icon icon="plus" class="mr-2" />Add Appointment
+    <appointment-list :appointments="appointments" />
   </div>
 </template>
 
 <script>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
+
+import AppointmentList from "./components/AppointmentList";
 
 export default {
-  name: 'MainApp',
+  name: "MainApp",
   data: function() {
     return {
-      title: 'Appointment List'
-    }
+      title: "Appointment List",
+      appointments: []
+    };
   },
   components: {
-    FontAwesomeIcon
+    FontAwesomeIcon,
+    AppointmentList
+  },
+  mounted() {
+    axios
+      .get("./data/appointments.js")
+      .then(response => (this.appointments = response.data));
   }
-}
+};
 </script>
